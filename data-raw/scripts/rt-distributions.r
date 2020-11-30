945fb69cc6ab0b5691d81067acbcb92333e3826b
seabbs <s.e.abbott12@gmail.com>
2020-11-30 11:00:02 +0000

    check reporting delay

--- data-raw/scripts/rt-distributions.r
+++ data-raw/scripts/rt-distributions.r
@@ -27,7 +27,7 @@ saveRDS(incubation_period, here::here("data-raw", "data", "incubation_period.rds
 # use an approximate delay distribution based on local knowledge
 # truncate the distribution at 30 days
 reporting_delay <- list(mean = 1.25,
-                        means_sd = 0.05,
+                        mean_sd = 0.05,
                         sd = 1,
                         sd_sd = 0.05,
                         max = 30)
