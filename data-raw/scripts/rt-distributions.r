6e9bb661c19e0a93e3c790b559f77a5b3618242c
seabbs <s.e.abbott12@gmail.com>
2020-11-30 10:55:35 +0000

    update report delay to have max truncation and save

--- data-raw/scripts/rt-distributions.r
+++ data-raw/scripts/rt-distributions.r
@@ -29,7 +29,8 @@ saveRDS(incubation_period, here::here("data-raw", "data", "incubation_period.rds
 reporting_delay <- list(mean = 1.25,
                         means_sd = 0.05,
                         sd = 1,
-                        sd_sd = 0.05)
+                        sd_sd = 0.05,
+                        max = 30)
 saveRDS(reporting_delay, here::here("data-raw", "data", "reporting_delay.rds"))
 
 print("Reporting delay parameters (log normal)")
